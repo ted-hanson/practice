9b5a12d792f791c8228df7f5cd6993c1cc3c9025
Ted Hanson <ted@ted.local>
2015-03-11 19:11:57 -0700

    pretty dang clean number to dictionary

--- prob17.py
+++ prob17.py
@@ -1,4 +1,5 @@
 words = {
+    0: 'zero ',
     1: 'one ',
     2: 'two ',
     3: 'three ',
@@ -33,32 +34,42 @@ words = {
     10**15: 'quadrillion '
   }
 
+def addAnd(left, right, should_and):
+  return left + ('and ' if right != '' and should_and else '') + right
+
 def digits(n):
   global words
   s = str(n)
   i = len(s)
-  if i == 0 or s == '0':
+  if i == 0 or int(s) == 0:
     return ''
   elif i > 3:
     # map digits to hundreds correctly: 123|457 -> 1:get next 3 digits, 2: get next 2 digits, 3: only get 1 digit, 4:3, 5:2, 6:1
     lead = (i + 2) % 3 + 1 
-    # get number before factor of 10**3
+    # get number on the left side of 10**3
     left = digits(int(s[:lead])) + words[10**(i-lead)]
-    # get number after factor of 10**3
+    # get number on the right side of 10**3
     right = digits(int(s[lead:]))
-    words[n] = left + ('and ' if right != '' and len(str(int(s[lead:]))) <= 2 else '') + right
-    return words[n]
-  # take custom <3 digits before automating
+    # don't add and between 1000's and 100's
+    word = addAnd(left, right, int(s[lead:]) <= 99)
+    # Don't overwrite defaults i.e. 1000
+    if n not in words:
+      words[n] = word
+    return word
+  # take custom digits <= 3 before automating
   elif n in words:
     return words[n]
   # make word for two digit numbers
   elif i == 2:
     return words[int(s[0])*10] + digits(int(s[1:])) 
+  # make word for 3 digit numbers
   elif i == 3:
-    u = words[int(s[0])] + 'hundred '
-    v = digits(int(s[1:])) 
-    if v != '':
-      u += 'and '
-    return u + v
+    # hundreds digit
+    left = words[int(s[0])] + 'hundred '
+    # rest of number
+    right = digits(int(s[1:])) 
+    return addAnd(left, right, True)
 
-print sum([len(digits(i).replace(' ', '')) for i in range(1, 1001)])
+nums = [digits(i).replace(' ', '') for i in range(1, 1001)]
+print nums
+print sum(map(len, nums))
